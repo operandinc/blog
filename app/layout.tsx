b8a5eadfa0e5b28f1d811f1ce18f2e6e35b5e171
Morgan Gallant <morgan@morgangallant.com>
2023-02-04 17:19:04 -0500

    getting going on blog
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,46 @@
-import './globals.css'
+import { Inter } from "@next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "optional",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="en" className={`${inter.variable}`}>
       <head />
-      <body>{children}</body>
+      <body className="container mx-auto px-4">
+        <header>
+          <nav className="flex justify-between items-center p-4 border-b border-gray-200">
+            <div className="flex items-center">
+              <Image
+                src="/logo.png"
+                alt="Operand Logo"
+                width={50}
+                height={50}
+              />
+              <h1 className="text-xl font-medium ml-2">Operand Blog</h1>
+            </div>
+            <Link href="https://operand.ai" className="text-gray-600">
+              Go to Homepage
+            </Link>
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer>
+          <p className="text-center text-gray-500 py-4">
+            © {new Date().getFullYear()} Operand, Inc.
+          </p>
+        </footer>
+      </body>
     </html>
-  )
+  );
 }
